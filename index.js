6f6083ad0d74145ada772998bd7f449bd87ff34b
Jestthecoder85 <majesticwilson85@gmail.com>
2022-10-23 13:19:29 -0400

    finish game

--- index.js
+++ index.js
@@ -1,9 +1,28 @@
 // Your Code Here
 let userName = window.prompt('Please enter your name:')
-for(let i = 0; i < questions.length; i++) {
-    let question = questions[i]
-    let userAnswer = window.prompt(question.text)
-    if(userAnswer === question.correctAnswer) {
-        console.log('Correct')
+let userScore = 0
+let playAgain = true
+
+function playGame() {
+    for(let i = 0; i < questions.length; i++) {
+        let question = questions[i]
+        let userAnswer = window.prompt(question.text)
+        if(userAnswer === question.correctAnswer) {
+        userScore = useScore + 10
+        }
     }
+
+    window.alert('Your score is: '+userScore)
+
 }
+
+while (playAgain === true) {
+    playGame()
+    let userChoice = window.prompt('Would you like to play again? Answer yes or no. ')
+    if (userChoice = 'yes') {
+        playagain = true
+    }else{
+        playagain = false
+        window.alert('Thanks for playing the game!')
+    }
+}    
