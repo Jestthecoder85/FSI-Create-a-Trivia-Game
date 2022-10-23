26b57fdfa341824427265dedac9efc8634c1693b
Jestthecoder85 <majesticwilson85@gmail.com>
2022-10-23 12:18:58 -0400

    updated 2

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 // Your Code Here
-let userName = window.prompt ('Please enter your name:')
+let userName = window.prompt('Please enter your name:')
 for(let i = 0; i < questions.length; i++) {
     let question = questions[i]
-    let userAnswer = window.prompt (question.text)
+    let userAnswer = window.prompt(question.text)
     if(userAnswer === question.correctAnswer) {
         console.log('Correct')
     }
