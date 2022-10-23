2078348ddc2c4a13ed30347a8b1204c6f195f209
Jestthecoder85 <majesticwilson85@gmail.com>
2022-10-23 12:16:24 -0400

    updated game

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Your Code Here
 let userName = window.prompt ('Please enter your name:')
-for (let i = 0; i <questions.length; i++) {
+for(let i = 0; i < questions.length; i++) {
     let question = questions[i]
     let userAnswer = window.prompt (question.text)
     if(userAnswer === question.correctAnswer) {
